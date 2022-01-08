a1c2101f6bb189102d1a53a8b1b50aca90228c6b
Mark Baum <markmbaum@protonmail.com>
2022-01-08 10:01:00 -0500

    include short explanation in the readme, include very simple test that preindustrial conditions are at equilibrium

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using preCOPSE
 using Test
 
-@testset "preCOPSE.jl" begin
-    @test 1 < 2
+const pCO2₀ = 285e-6
+
+@testset "pre-industrial" begin
+    @test integrate(1e6, mac₀) ≈ pCO2₀
+    @test integrate(1e6, whak₀) ≈ pCO2₀
 end
